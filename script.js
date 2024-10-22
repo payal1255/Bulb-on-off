964f72ff647384dc466ce7b937829f3e93336bee
payal1255 <payalsulkhiya@gmail.com>
2024-10-22 16:12:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function on(){
+    const on = document.getElementById("img");
+    console.log(on.src)
+    on.src="on.png"
+    
+
+}
+function off(){
+    const off=document.getElementById("img");
+    off.src="off.png"
+}
